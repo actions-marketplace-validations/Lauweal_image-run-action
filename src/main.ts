104669fbf639e0806c8a54a48e31ae1ec13e7191
刘伟 <lemonpaimc@126.com>
2022-11-19 13:41:25 +0800

    feat(init): init

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,34 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+// import { Client, ConnectConfig } from 'ssh2';
+
+// function connect(options: ConnectConfig) {
+//   const conn = new Client();
+//   return new Promise<Client>((resolve, reject) => {
+//     conn.connect(options).on('error', () => {
+//       reject('连接异常');
+//     }).on('close', () => {
+//       reject('连接关闭');
+//     }).on('ready', () => {
+//       resolve(conn);
+//     });
+//   })
+// }
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const host = core.getInput('host') ?? '';
+    const username = core.getInput('username');
+    const password = core.getInput('password');
+    const image = core.getInput('image');
+    const name = core.getInput('name');
+    const args = core.getInput('args');
+    if (!username) throw new Error('请输入用户名')
+    if (!password) throw new Error('请输入密码')
+    if (!host) throw new Error('请输入ip')
+    if (!image) throw new Error('请输入镜像')
+    if (!name) throw new Error('请输入容器名')
+    core.info(JSON.stringify(args));
+    // const client = await connect({ username, password, host });
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
